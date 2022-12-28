7a8fd9a945f67b53e927e2b483dcba1aadddd2fc
Fyodor Ronginsky <fedorronginskij@gmail.com>
2022-12-28 14:00:51 +0300

    Add files via upload

---
+++ React JS/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1>
+            Это web-приложение создано в рамках обучения
+        </h1>
+    );
+};
+
+export default About;
