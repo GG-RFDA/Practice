4955828f0c2fd83fa2f13eacb20e198647c70f51
Fyodor Ronginsky <fedorronginskij@gmail.com>
2023-01-11 20:22:28 +0300

    Update ClassCounter.jsx

--- React JS/src/components/ClassCounter.jsx
+++ React JS/src/components/ClassCounter.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
 
-
+// Наследуем свойства от React компонента
 class ClassCounter extends React.Component {
-
+    // Создаём конструктор, принимающий параметрами пропсы
     constructor(props){
+        // используем super для вызова конструктора наследуемого класса
         super(props);
+        // Инициализируем состояние
         this.state = {
+            // Создаём поле "count" (счётчик)
             count: 0
         }
+        // Используем метод bind для привязки контекста и передаём this
         this.increment = this.increment.bind(this);
         this.decrement = this.decrement.bind(this);
     }
 
     increment(){
+        /*
+         * Для изменения состояния вызываем функцию setState, куда передаём новые состояния с изменёнными значениями.
+         * Значение счётчика увеличивается на единицу.
+         */
         this.setState({count: this.state.count + 1})
 
     }
 
     decrement(){
+        // Значение счётчика уменьшается на единицу
         this.setState({count: this.state.count - 1})
     }
 
     render(){
         return (
+            // Поскольку находимся внутри класса, для обращения к свойствам необходимо использовать ключевое слово this
             <div>
                 <h1>{this.state.count}</h1>
                 <button onClick={this.increment}>Increment</button>
@@ -31,5 +41,7 @@ class ClassCounter extends React.Component {
         )
     }
 }
-
+// Экспортируем класс
 export default ClassCounter;
+
+// Классовые компоненты являются устаревшим подходом. Рекомендуется использовать функциональные компоненты и хуки
